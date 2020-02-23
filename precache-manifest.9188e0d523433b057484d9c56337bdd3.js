b9e7215b91aa8d7a3cdfa53cba13544cc9fbab6c
Alexander Khomenko <homenkosasha13@gmail.com>
2020-02-23 15:53:19 +0200

    Updates

---
+++ precache-manifest.9188e0d523433b057484d9c56337bdd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239798b3e6346f23af154e87f7561ba9",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "241bbc75c9e0e4e2554d",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.9ab86875.chunk.css"
+  },
+  {
+    "revision": "0c6e6f08b2c440072d36",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.de0b5760.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.de0b5760.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "241bbc75c9e0e4e2554d",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.6b0aed1b.chunk.js"
+  },
+  {
+    "revision": "e68e18fb228ad198d7aa",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.fca25aed.js"
+  }
+]);
